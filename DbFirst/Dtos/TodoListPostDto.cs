f72c984c6e0b6ed4c1c9637bb5a2db13b9ab31e7
Len Shi <lenshih@jquan.com.tw>
2025-09-21 19:27:10 +0800

    新增 TodoListPostDto 類別的資料驗證功能，並初始化 UploadFile 屬性

--- DbFirst/Dtos/TodoListPostDto.cs
+++ DbFirst/Dtos/TodoListPostDto.cs
@@ -1,10 +1,22 @@
-﻿namespace DbFirst.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbFirst.Dtos
 {
     public class TodoListPostDto
     {
+        //加入資料驗證功能
+        [Required]
+        //[EmailAddress(ErrorMessage ="請輸入電子信箱")]
+        [StringLength(3)]
+        [RegularExpression("[a-z]")]
         public string Name { get; set; }
         public bool Enable { get; set; }
         public int Orders { get; set; }
         public List<UploadFilePostDto> UploadFile { get; set; }
+
+        public TodoListPostDto()
+        {
+            UploadFile = new List<UploadFilePostDto>();
+        }
     }
 }
